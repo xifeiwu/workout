4b39da81f9e22806bf24f149b3b41b8746bdd5ec
xifei <xifei@nfs.iscas.ac.cn>
2014-01-25 20:14:06 +0800

    add netfilter in server-manage

--- server-manage/chkconfig.sh
+++ server-manage/chkconfig.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 SERVICES2CLOSE=(cups vncserver rpcbind)
-SERVICES2OPEN=(httpd vsftpd)
+SERVICES2OPEN=(httpd vsftpd auditd)
 for service in ${SERVICES2CLOSE[@]}; do
     chkconfig ${service} off
     if [ $? -eq 0]; then
@@ -13,3 +13,12 @@ for service in ${SERVICES2OPEN[@]}; do
         echo "service ${service} on"
     fi
 do
+yum -y install setools
+yum -y install setroubleshoot*
+chcon -R -u system_u -t httpd_sys_content_t /home/cos/website/
+setsebool -P httpd_read_user_content 1
+
+#chcon -R -t ftpd_sys_content_t /home/cos/ftp-15
+setsebool -P ftpd_use_passive_mode 1
+setsebool -P allow_ftpd_full_access 1
+setsebool -P ftp_home_dir 1
